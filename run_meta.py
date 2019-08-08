7ddc7084e96359a5305937d56bd219a772f1b01c
Yaoyao <yyliu@next-gpu1.d2.comp.nus.edu.sg>
2019-08-08 16:44:31 +0800

    third commit

--- run_meta.py
+++ run_meta.py
@@ -1,18 +1,10 @@
 import os
 
 def run_exp(num_batch=1000, shot=1, query=15, lr=0.0001, lr2=0.001, lr3=0.00001, base_lr=0.01, update_step=10, gamma=0.5):
-    #num_batch = 1000
     max_epoch = 100
-    #shot = 1
-    #query = 15
     way = 5
-    #lr = 0.0001
-    #lr2 = 0.001
     step_size = 10
-    #gamma = 0.5
     gpu = 0
-    label = 'exp17'
-    init_weights = '/BS/sun_project_multimodal/work/yyliu_project/lcc-new/FEAT-New/pre_train/1shot-5way/net=1/bs=128-step=30-gamma=0.20/max_acc.pth'
     
     the_command = 'python3 main.py' \
         + ' --max_epoch=' + str(max_epoch) \
@@ -21,15 +13,15 @@ def run_exp(num_batch=1000, shot=1, query=15, lr=0.0001, lr2=0.001, lr3=0.00001,
         + ' --train_query=' + str(query) \
         + ' --way=' + str(way) \
         + ' --meta_lr1=' + str(lr) \
-        + ' --meta_lr1=' + str(lr2) \
+        + ' --meta_lr2=' + str(lr2) \
         + ' --step_size=' + str(step_size) \
         + ' --gamma=' + str(gamma) \
         + ' --gpu=' + str(gpu) \
         + ' --base_lr=' + str(base_lr) \
-        + ' --update_step=' + str(update_step) \
-        + ' --meta_label=' + label \
-        + ' --init_weights=' + init_weights
+        + ' --update_step=' + str(update_step) 
 
-    os.system(the_command)
+    os.system(the_command + ' --phase=meta_train')
+    os.system(the_command + ' --phase=meta_eval')
 
 run_exp(num_batch=100, shot=1, query=15, lr=0.0001, lr2=0.001, lr3=0.00001, base_lr=0.01, update_step=100, gamma=0.5)
+run_exp(num_batch=100, shot=5, query=15, lr=0.0001, lr2=0.001, lr3=0.00001, base_lr=0.01, update_step=100, gamma=0.5)
